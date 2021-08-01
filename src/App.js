937b57819b35185fe99897a14b1138eefae705a5
Sergii Bogush <sergbogush@gmail.com>
2021-08-01 16:47:05 +0300

    add Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Switch, Route } from "react-router-dom";
 import Container from "./Components/Container/Container";
 import AppBar from "./Components/AppBar/AppBar";
+import HomeView from "./views/HomeView";
+import MoviesView from "./views/MoviesView";
 
 function App() {
   return (
     <Container>
       <AppBar />
+      <Route path="/" exact>
+        <HomeView />
+      </Route>
+      <Route path="/movies">
+        <MoviesView />
+      </Route>
     </Container>
   );
 }
