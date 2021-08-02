496e9ae023a9b41566386f91a8babab00daafdb4
Sergii Bogush <sergbogush@gmail.com>
2021-08-02 12:41:23 +0300

    add  movie detail - poster, title, genres,

--- src/views/MovieDetailView.js
+++ src/views/MovieDetailView.js
@@ -8,20 +8,45 @@ export default function MovieDetailView() {
   console.log(movieId);
 
   useEffect(() => {
-    api.getMovieById(movieId).then(setMovie);
+    api
+      .getMovieById(movieId)
+      .then((data) => ({
+        ...data,
+        release_date: data.release_date
+          ? data.release_date.slice(0, 4)
+          : "No Date",
+        poster_path: data.poster_path
+          ? `https://image.tmdb.org/t/p/w300${data.poster_path}`
+          : "../no-poster.jpg",
+        vote_average: data.vote_average * 10,
+      }))
+      .then(setMovie);
   }, [movieId]);
 
   console.log(movie);
   //   const { title, genres, poster_path } = movie;
 
   return (
-    movie && (
-      <div>
-        <div></div>
+    <>
+      {movie && (
         <div>
-          <h2>{movie.title}</h2>
+          <div>
+            <img src={movie.poster_path} alt={movie.title} />
+          </div>
+          <div>
+            <h2>{movie.title}</h2>
+            <p>User Score: {movie.vote_average}%</p>
+            <h3>Overview</h3>
+            {movie.overview && <p>{movie.overview}</p>}
+            <h4>Genres</h4>
+            <ul>
+              {movie.genres.map(({ id, name }) => (
+                <li key={id}>{name}</li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-    )
+      )}
+    </>
   );
 }
